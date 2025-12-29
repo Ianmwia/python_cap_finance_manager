93dd8ca74dd220bdd2aa326fdad5387aaa420c51
Mwia <iansnr10@gmail.com>
2025-12-29 09:20:31 +0300

    creating the database connection

---
+++ database.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://iansnr10_db_user:5RX8IPaawRZm9kxK@financemanagercluster.vxpefms.mongodb.net/?appName=financemanagercluster"
+
+client = MongoClient(MONGO_URI)
+
+db = client["finance_manager"]
+
+income_collection = db["incomes"]
+expense_collection = db["expenses"]
