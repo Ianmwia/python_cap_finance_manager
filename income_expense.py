aa7fe7a4bebfbde75b3032fa56412e9a5b4337a1
Mwia <iansnr10@gmail.com>
2025-12-29 15:35:18 +0300

    adding date and note to the income and expense

--- income_expense.py
+++ income_expense.py
@@ -1,16 +1,38 @@
+from datetime import datetime
 class Income():
-    def __init__(self, amount):
+    def __init__(self, amount, note='', date = None):
         self.amount = amount
+        self.note = note
+        if date is not None:
+            self.date = date
+        else:
+            self.date = date.datetime.now()
+        
     def to_dict(self):
-        return {"amount": self.amount}
+        return {
+            "amount": self.amount,
+            "date": self.date,
+            "note": self.note
+            }
     def __str__(self):
         return f'Income for the month is {self.amount}'
     
 class Expense():
-    def __init__(self, amount, category):
+    def __init__(self, amount, category, note='', date = None):
         self.amount = amount
         self.category = category
+        self.note = note
+        if date is not None:
+            self.date = date
+        else:
+            self.date = date.datetime.now()
+        
     def to_dict(self):
-        return {"amount": self.amount, "category": self.category}
+        return {
+            "amount": self.amount, 
+            "category": self.category,
+            "date": self.date,
+            "note": self.note
+            }
     def __str__(self):
         return f'You have spent {self.amount} on {self.category}'
