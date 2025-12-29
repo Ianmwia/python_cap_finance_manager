9f556f435324da5dd20a31301187c5860b00a3de
Mwia <iansnr10@gmail.com>
2025-12-29 09:23:19 +0300

    removing instances and adding pymongo dict to convert data to dictionaries for mongodb

--- income_expense.py
+++ income_expense.py
@@ -1,18 +1,16 @@
 class Income():
     def __init__(self, amount):
         self.amount = amount
+    def to_dict(self):
+        return {"amount": self.amount}
     def __str__(self):
         return f'Income for the month is {self.amount}'
     
-income = Income(30000)
-print(income)
-
 class Expense():
     def __init__(self, amount, category):
         self.amount = amount
         self.category = category
+    def to_dict(self):
+        return {"amount": self.amount, "category": self.category}
     def __str__(self):
         return f'You have spent {self.amount} on {self.category}'
-
-expense = Expense(3000, "Food")
-print(expense)
