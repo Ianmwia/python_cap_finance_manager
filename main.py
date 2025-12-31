bcfb6ac48ac636d9638b58e1aa5f991913bdb2d3
Mwia <iansnr10@gmail.com>
2025-12-31 12:42:05 +0300

    fixing the date issue and multi user creation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from database import users_collection
 
 #ask for username
-username = input("Please input your username: ")
+username = input("Please input your username: ").strip()
 
 #check if user exists , if not create one
 user = users_collection.find_one({"username": username})
@@ -22,29 +22,36 @@ choice = input("Do You want to add an INCOME or EXPENSE, write 'income' for INCO
 if choice == 'income':
     income_amount = float(input('Enter an Income Amount: '))
     note_input = input('Add a note: ')
-    date_input = input('Enter a date in the format DD-MM-YYYY: ')
+    while True:
+        date_input = input('Enter a date in the format DD-MM-YYYY: ').strip()
+        if not date_input:
+            print("Date is required! Please enter a date")
+            continue
+        try:
+            date = datetime.strptime(date_input, "%d-%m-%Y")
+            break
+        except ValueError:
+            print("invalid date format! use DD-MM-YYYY")
 
-    if date_input:
-        date = datetime.strptime(date_input, "%d-%m-%Y")
-    else:
-        date = None
-
-    income = Income(amount = income_amount, note = note_input, date = date_input)
+    income = Income(amount = income_amount, note = note_input, date = date)
     e.add_income(income)
 
 elif choice == 'expense':
     expense_amount = float(input('Add an expense Amount: '))
     category_input = input('Add an expense category: ')
     note_input = input('Add a note: ')
-    date_input = input('Enter a date in the format DD-MM-YYYY: ')
+    while True:
+        date_input = input('Enter a date in the format DD-MM-YYYY: ').strip()
+        if not date_input:
+            print("Date is required! Please enter a date")
+            continue
+        try:
+            date = datetime.strptime(date_input, "%d-%m-%Y")
+            break
+        except ValueError:
+            print("invalid date format! use DD-MM-YYYY")
 
-
-    if date_input:
-        date = datetime.strptime(date_input, "%d-%m-%Y")
-    else:
-        date = None
-
-    expense = Expense(amount = expense_amount, category = category_input, note = note_input, date = date_input)
+    expense = Expense(amount = expense_amount, category = category_input, note = note_input, date = date)
     e.add_expense(expense)
 else:
     print("Invalid input choice , write 'income' for INCOME and 'expense' for Expense")
