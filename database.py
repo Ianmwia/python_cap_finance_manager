d5dc2c2c3170c893a85ce48a13e3bec04ec2db3a
Mwia <iansnr10@gmail.com>
2025-12-31 13:09:20 +0300

    remove hard coded database url and replace with dotenv

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-MONGO_URI = "mongodb+srv://iansnr10_db_user:5RX8IPaawRZm9kxK@financemanagercluster.vxpefms.mongodb.net/?appName=financemanagercluster"
+load_dotenv()
 
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 
 db = client["finance_manager"]
@@ -9,3 +12,6 @@ db = client["finance_manager"]
 income_collection = db["incomes"]
 expense_collection = db["expenses"]
 users_collection = db["users"]
+
+#total_users = users_collection.count_documents({})
+#print(total_users)
